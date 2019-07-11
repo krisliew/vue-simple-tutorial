d383e7b8e84afbff4c7e61c10d99aaa10c3824e6
Jing <39853992+soonjing@users.noreply.github.com>
2019-07-11 11:36:43 +0800

    vue init webpack vue-tutorial

---
+++ src/components/Form.vue
@@ -0,0 +1,46 @@
+<template>
+    <!-- Part 6 -->
+    <form v-on:submit.prevent="formSubmit">
+        <h1>h1: This form is a Child Component (Form.vue) </h1>
+
+        <input type="text" v-model="formInput">
+        <p> {{ formInput }}</p>
+
+        <button type="submit">Submit</button>
+        <p id="errorMsg"> {{ ErrorMsg }}</p>      
+    </form>
+</template>
+
+<script>
+
+export default {
+  name: 'myForm',
+  data () {
+    return {
+      formInput:'',
+      ErrorMsg:''
+    }
+  },
+  methods:{
+    formSubmit(){
+        if(this.formInput.length < 1){
+            this.ErrorMsg = 'You have clicked the malfunctioned submit button!'
+        }else{
+            this.ErrorMsg = ''
+            //Can add a post method here
+        }     
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1{
+    font-size:14px;
+}
+#errorMsg{
+    color:red;
+    font-weight: 300;
+    font-size:13px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>    
+    <!-- Part 1 -->
+    <h1>h1:{{ msg }} (HelloWorld.vue)</h1>
+    
+    <!-- Part 2 -->
+    <p> Question: Do you like spicy food? </p>
+    <button type="button" @click="changeState"> {{ Btn }} </button>
+    <button type="button" @click="elseState"> idk... </button>
+    
+    <!-- Part 3 -->
+    <p v-if="Btn=='Yes'"> Ans: I love spicy food too! </p>
+    <p v-else-if="Btn=='No'"> Ans: You are missing out... </p>
+    <p v-else> Ans: You don't know? Lets go eat now! </p>    
+
+    <!-- Part 4 -->
+    <h2> Vue for loop </h2>
+    <ul v-for="food in foods" :key="food.foodId" >
+      <li> Food ID {{food.foodId }} is {{ food.foodName}}</li>
+    </ul>
+
+    <!-- Part 5 -->
+    <button @click="showForm"> Show Form </button>
+    <myForm v-show="Display==true"/>
+  </div>
+</template>
+
+<script>
+import myForm from './Form'
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    'myForm':myForm
+  },
+  data () {
+    return {
+      msg: 'Welcome to your first Vue.js App ^_^',
+      Btn : 'Yes',
+      Display:false, // for myForm,
+
+      foods:[
+        {
+          foodId : 1,
+          foodName : "Nasi Lemak"
+        },
+        {
+          foodId : 2,
+          foodName : "Nasi Putih Supertanker"
+        },
+        {
+          foodId : 3,
+          foodName : "Lasagna"
+        }
+      ]
+    }
+  },
+  methods:{
+    changeState(){
+      if(this.Btn == 'Yes'){
+        this.Btn = 'No'
+      }else{
+        this.Btn = 'Yes'
+      }
+    },
+    elseState(){
+      this.Btn = 'Yes!'      
+    },
+    showForm(){
+      this.Display = true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
